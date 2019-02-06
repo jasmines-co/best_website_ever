9666cbc16874ac29d0e15a1b5fa77f6d748a13ed
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-05 19:31:55 -0800

    update tagline

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Index = () => (
     <div>
       <Header />
       <img className="logo" src="/static/dark-logo.png" alt="logo" />
-      <p className='tagline'>Bitcoin at your Fingertips</p>
+      <p className='tagline'>Bitcoin Cash at your Fingertips</p>
     </div>
   )
   
