fcd49820e6e941d9d2734aefb8c40120b71f2dbb
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-05 16:47:09 -0800

    added 404 page

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import Header from "../components/shared/Header";
+
+
+// This is the Link API
+import Link from 'next/link';
+
 export default class Error extends React.Component {
   static getInitialProps({ res, err }) {
     const statusCode = res ? res.statusCode : err ? err.statusCode : null;
@@ -8,11 +14,15 @@ export default class Error extends React.Component {
 
   render() {
     return (
-      <p>
+    <div>
+     <Header/>
+     <img className="logo" src="/static/error_coin.png" alt="logo" />
+      <p className="error-page">
         {this.props.statusCode
           ? `An error ${this.props.statusCode} occurred on server`
           : 'An error occurred on client'}
       </p>
+      </div>
     )
   }
 }
