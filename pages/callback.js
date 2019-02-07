6d39dae0ca24dd35edacb6ae7d5ba39e43c0797b
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-07 13:40:20 -0800

    update callback js to display state

--- pages/callback.js
+++ pages/callback.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Header from "../components/shared/Header";
+import auth0Client from '../services/auth0';
 
-const Callback = () => (
-    <div>
-      <Header />
-      <h1>You are logged in</h1>
-       
-    </div>
-  )
-  
-  export default Callback;
+
+export default class Callback extends React.Component {
+    componentDidMount(){
+        auth0Client.handleAuthentication();
+    }
+
+    render(){
+        return(  
+        <div>
+            <Header />
+            <h1>You are logged in</h1> 
+          </div> 
+        )
+        }
+    }
