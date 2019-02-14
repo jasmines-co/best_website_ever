68d469e9cefc38e48d9993eab0b208786c5fb2c4
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-14 13:58:00 -0800

    troubleshooting heroku

--- env-config.js
+++ env-config.js
@@ -3,5 +3,6 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   'process.env.BASE_URL': prod ? 'https://toshi-text.herokuapp.com/' : 'http://localhost:3000',
-  'process.env.NAMESPACE': 'https://toshi-text.herokuapp.com/'
+  'process.env.NAMESPACE': 'https://toshi-text.herokuapp.com/',
+  'process.env.CLIENT_ID': 'z5UssdBfYc7R5OOEPBprycmfXrcKIxD1'
 }
