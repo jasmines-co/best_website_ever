b88500a9b51019ac0acf112f7be2914b91c89d8a
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-18 10:09:37 -0800

    trouble shoot twilio

--- server.js
+++ server.js
@@ -145,6 +145,24 @@ client.messages
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
   });
+
+  server.post('/', (req, res) => {
+    const twiml = new MessagingResponse();
+  
+    if (req.body.Body == 'hello') {
+      twiml.message('Hi!');
+    } else if (req.body.Body == 'bye') {
+      twiml.message('Goodbye');
+    } else {
+      twiml.message(
+        'No Body param match, Twilio sends this in the request to your server.'
+      );
+    }
+  
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  });
+  
   
 
   const faviconOptions = {
