0e057294b3c1514e4ffbc1b63dba78625f4fdeff
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-02-18 10:12:24 -0800

    troubleshooting heroku errors

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.prepare()
   // });
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 client.messages
   .create({
